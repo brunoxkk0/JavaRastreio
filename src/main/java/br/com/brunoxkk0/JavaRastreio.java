c3763afe3176054dc5c99b6d54ba1d30e9209b5d
bruno <cocacola_bruno@hotmail.com>
2019-10-29 19:57:51 -0300

    Atualizando código

--- src/main/java/br/com/brunoxkk0/JavaRastreio.java
+++ src/main/java/br/com/brunoxkk0/JavaRastreio.java
@@ -1,5 +1,6 @@
 package br.com.brunoxkk0;
 
+import br.com.brunoxkk0.utils.Parser;
 import br.com.brunoxkk0.utils.WebHelper;
 
 public class JavaRastreio {
@@ -7,7 +8,8 @@ public class JavaRastreio {
     public static void main(String[] args) {
         WebHelper webHelper = new WebHelper();
 
-        System.out.println(webHelper.post("https://www2.correios.com.br/sistemas/rastreamento/resultado_semcontent.cfm","objetos=PS240269255BR", "null"));
+        new Parser(webHelper.post("https://www2.correios.com.br/sistemas/rastreamento/resultado_semcontent.cfm","objetos=PS240269255BR", "null"));
+        //System.out.println(webHelper.post("https://www2.correios.com.br/sistemas/rastreamento/resultado_semcontent.cfm","objetos=PS240269255BR", "null"));
 
     }
 }
